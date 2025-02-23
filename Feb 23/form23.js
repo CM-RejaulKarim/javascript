fde4457152de22df23622188312f55902f5f456e
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-02-23 19:03:34 +0600

    Class-Feb-23

---
+++ Feb 23/form23.js
@@ -0,0 +1,19 @@
+function formSubmit() {
+
+    let rName = document.getElementById('name').value;
+    let rContact = document.getElementById('name').value;
+    let rRemark = document.getElementById('name').value;
+    let gen = document.querySelector('input[name="gender"]:checked');
+    let course = document.querySelectorAll('input[name="gender"]:checked');
+    let location = document.getElementById('name').value;
+
+    let course =
+
+
+
+
+
+
+
+
+}
